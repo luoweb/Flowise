bc03d9cd555c1ff8c997f2e6a008d9ea28ef7689
robin <hongxing525@126.com>
2024-05-16 01:05:40 +0800

    fix(build): sed error

--- packages/custom.sh
+++ packages/custom.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 
 # logo
-# sed -i "" 's#flowise#blockai#g' ../packages/ui/src/ui-component/extended/Logo.jsx
-sed -i "" 's#/assets/images/flowise_logo#/assets/images/blockai_logo#g' ../packages/ui/src/ui-component/extended/Logo.jsx
+# sed -i "" "s#flowise#blockai#g" ../packages/ui/src/ui-component/extended/Logo.jsx
+# alias xsed="sed -i """
+alias xsed="sed -i"
+xsed "s#/assets/images/flowise_logo#/assets/images/blockai_logo#g" ../packages/ui/src/ui-component/extended/Logo.jsx
 
 # avatar
 find ../packages -type f -maxdepth 3 | grep -v "custom.sh" | xargs grep -o "Flowise - Low-code LLM apps builder"
-sed -i "" 's#Flowise - Low-code LLM apps builder#BloclAI - LLM apps builder#g' ../packages/ui/public/index.html
-sed -i "" 's#Flowise - Low-code LLM apps builder#BloclAI - LLM apps builder#g' ../packages/ui/index.html
+xsed "s#Flowise - Low-code LLM apps builder#BloclAI - LLM apps builder#g" ../packages/ui/public/index.html
+xsed "s#Flowise - Low-code LLM apps builder#BloclAI - LLM apps builder#g" ../packages/ui/index.html
 
 # url
 find ../packages -type f -maxdepth 3 | grep -v "custom.sh"  | xargs grep -o "https://flowiseai.com" 
-sed -i "" 's#https://flowiseai.com#https://roweb.cn#g' ../packages/ui/index.html
-sed -i "" 's#https://flowiseai.com#https://roweb.cn#g' ../packages/ui/public/index.html
-sed -i "" 's#https://flowiseai.com#https://roweb.cn#g' ../package.json
-sed -i "" 's#https://flowiseai.com#https://roweb.cn#g' ../packages/ui/package.json
-sed -i "" 's#https://flowiseai.com#https://roweb.cn#g' ../packages/server/package.json
-sed -i "" 's#https://flowiseai.com#https://roweb.cn#g' ../packages/components/package.json
-sed -i "" 's#https://flowiseai.com#https://roweb.cn#g' "../packages/server/marketplaces/chatflows/WebPage QnA.json"
+xsed "s#https://flowiseai.com#https://roweb.cn#g" ../packages/ui/index.html
+xsed "s#https://flowiseai.com#https://roweb.cn#g" ../packages/ui/public/index.html
+xsed "s#https://flowiseai.com#https://roweb.cn#g" ../package.json
+xsed "s#https://flowiseai.com#https://roweb.cn#g" ../packages/ui/package.json
+xsed "s#https://flowiseai.com#https://roweb.cn#g" ../packages/server/package.json
+xsed "s#https://flowiseai.com#https://roweb.cn#g" ../packages/components/package.json
+xsed "s#https://flowiseai.com#https://roweb.cn#g" "../packages/server/marketplaces/chatflows/WebPage QnA.json"
